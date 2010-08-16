75eaae9e23304edc8874da8314790c32739fc5e2
aloucks <aloucks@cofront.net>
2010-08-16 18:04:53 -0400

    junk

---
+++ src/net/cofront/audio/tagger/BitTest2.java
@@ -0,0 +1,20 @@
+package net.cofront.audio.tagger;
+
+import java.util.Random;
+
+public class BitTest2 {
+	
+	public static final byte[] b64 = new byte[] {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
+		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+		'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+	};
+	
+	public static void main(String arg[]) {
+		String s = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+	
+		byte[] block = new byte[3];
+		
+	}
+}
